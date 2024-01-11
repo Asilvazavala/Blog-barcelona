7fe87ce2f079b357bfa199ab882ff5a54ba6a551
Asilvazavala <asilvazavala@gmail.com>
2024-01-11 10:38:48 -0600

    modified server

--- Backend/api/README.md
+++
@@ -1,6 +0,0 @@
-## Objetivos del Proyecto
-
-- Generar un sitio web para consultar, crear, actualizar y eliminar (CRUD)
-- Construir dicha página utlizando en el Backend: Node, Express y Sequelize
- y en el Frontend: React, Redux y CSS Modules.
-
--- Backend/api/src/controllers/getAllPublications.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const { Publications } = require('../db');
-
-const getAllPublications = async () => {
-  const allPublications = await Publications.findAll()
-  return (allPublications);
-};
-
-module.exports = getAllPublications;
